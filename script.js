cd9499c7358d81560f105fb0e77727b55d0e76a2
NoorSonjoq <noorsonjoq97@gmail.com>
2025-01-03 20:55:55 +0300

    Noor

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+// Task1
 // let names = ["ahmad","anas","omar","ali",["wedad","layan","esraa"]]
 // names = names.flat(); 
 // for (let i=0; i< names.length;i++)
 //     console.log(names[i].toUpperCase())
-
-
+// ------\\
 // solution 2
+// let a=["ahmad","anas","omar","ali"]
+// let b=["wedad","layan","esraa"] 
+// let c = a.concat(b);
+// for (let i = 0; i < c.length; i++) {
+//     console.log(c[i].toUpperCase());
+// }
+// --------------------------------------------------------------------------------------------------------------
+// Task2 Array
+// JavaScript Array flat() 
 
-let a=["ahmad","anas","omar","ali"]
-let b=["wedad","layan","esraa"] 
-let c = a.concat(b);
-for (let i = 0; i < c.length; i++) {
-    console.log(c[i].toUpperCase());
-}
+let Arrays = [["salma","sama"],["Sara","Noor"]];
+let newArrays  = Arrays .flat();
+console.log(newArrays)
